f0deef0425233f822b800a296aae75de0e8ccc05
Logan Gingerich <logangingerich@yahoo.com>
2021-08-24 13:16:02 -0600

    Update SDK Version and comments

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,26 @@ var express = require('express');
 var router = express.Router();
 var express = require('express');
 var router = express.Router();
+
+// Import the WorkOS package.
 const WorkOS = require('@workos-inc/node').default;
 
+// Initialize the WorkOS client with your WorkOS API Key, pulled from .env.
 const client = new WorkOS(process.env.WORKOS_API_KEY);
-const domain = "$YOUR_DOMAIN";
-const redirectURI = "$YOUR_REDIRECT_URI";
-const projectID = "$YOUR_PROJECT_ID";
+
+// Use domain associated to the organization in which your SSO Connection resides.
+// Alternatively, if your Organization has multiple SSO Connections within it,
+// you can pass the Connection ID instead of the domain.
+const domain = "dotnet.com";
+
+// Set the redirect URI to whatever URL the end user should land on post-authentication
+// You'll also want to ensure that the redirect URI you use is included in your
+// allow list in the WorkOS Dashboard.
+const redirectURI = "http://localhost:3000/callback";
+
+// Store the Client ID, pulled from .env and found in the Configuration section
+// of the WorkOS Dashboard.
+const clientID = process.env.WORKOS_CLIENT_ID;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,21 +30,32 @@ router.get('/', function(req, res, next) {
 
 /* GET login page */
 router.get('/login', (_req, res) => {
+  // Make a call to getAuthoirzationURL, passing the domain (or Connection ID),
+  // the redirect URI (optional, otherwise it will use your default set in the Dashboard)
+  // and the clientID. Store the resulting URL in a `url` variable.
   const url = client.sso.getAuthorizationURL({
     domain,
     redirectURI,
-    projectID,
+    clientID,
   });
+
+  // Redirect the user to the url generated above.
   res.redirect(url);
 });
 
 /* GET callback page */
 router.get('/callback', async (req, res) => {
+  // Capture and save the `code` passed as a querystring in the Redirect URI.
   const { code } = req.query;
-  const profile = await client.sso.getProfile({
+
+  // Make a call to getProfileAndToken and pass in the code (stored above) and
+  // the clientID. This will return a JSON user profile, stored here in `profile`.
+  const profile = await client.sso.getProfileAndToken({
     code,
-    projectID,
+    clientID,
   });
+
+  //Render the profile stored above.
   res.json(profile).send();
 });
 
